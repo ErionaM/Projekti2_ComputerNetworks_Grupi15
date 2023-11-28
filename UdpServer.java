30616f18e82710feb739ebf89afe87c56773e97d
ErandKurtaliqi <122090325+ErandKurtaliqi@users.noreply.github.com>
2023-11-28 12:41:45 +0100

    Update UdpServer.java

--- UdpServer.java
+++ UdpServer.java
@@ -1,19 +1,29 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
 public class UdpServer {
     public static void main(String[] args) throws IOException {
-
+        // 1. Të krijohet socket lidhja me server;
+        // Krijimi i soket lidhjes
         DatagramSocket ServerSocket = new DatagramSocket(5000);
         DatagramSocket ServerSocket2 = new DatagramSocket(5001);
 
+        // 4. Të jetë në gjendje të lexoje mesazhet që dërgohen nga klientët;
+        // Leximi i te dhenave
         byte [] leximiDhena = new byte[1024];
 
+        // Krijimi i pranuesit te paketes
         DatagramPacket pranuesiDhenave = new DatagramPacket(leximiDhena, leximiDhena.length);
 
+        // 3. Të pranojë kërkesat e pajisjeve që dërgojnë request
         ServerSocket.receive(pranuesiDhenave);
         ServerSocket2.receive(pranuesiDhenave);
 
+        // Extract i mesazhi
         String mesazhi = new String(leximiDhena, 0, pranuesiDhenave.getLength());
         String mesazhi2 = new String(leximiDhena, 0, pranuesiDhenave.getLength());
-
+        // Leximi/Shfaqja e mesazhit nga Klienti
         System.out.println("Mesazhi klientit: " + mesazhi);
         System.out.println("Mesazhi i klientit (pa priviligje): " + mesazhi2);
         System.out.println("IP e klientit: " + pranuesiDhenave.getAddress());
